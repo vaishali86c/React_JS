26fbdb62e67baa62802d139c1189e7ff8d19bba3
vaishali86c <vaishali86c@gmail.com>
2024-01-27 23:59:59 +0530

    Upload counter basic program

--- 02-counter/src/App.jsx
+++ 02-counter/src/App.jsx
@@ -1,41 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
 
-  let [counter, setCounter] = useState(15)
-
-  // let counter = 15;
-
-  const addValue= () => {
-    // console.log("value added", Math.random())
-    console.log("Clicked", counter);
-    // counter = counter + 1;
-    
-    setCounter(counter + 1);
-  }
-
-  const removeValue = () => {
-    setCounter(counter - 1);
+  function onClickHandler() {
+    setCount(count + 1);
   }
 
   return (
-    <>
-      <h1>Chai aur react</h1>
-      <h2>Counter value: {counter} </h2>
-      
-      <button
-      onClick={addValue} 
-      >Add value: {counter} </button>
-
-      <button
-      onClick={removeValue}
-      >Remove value: {counter} </button>
-
-    </>
-  ) 
+    <div>
+      <button onClick={onClickHandler}>counter {count}</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
