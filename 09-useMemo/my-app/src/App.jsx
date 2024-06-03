0c3f5440c6f7dcb245c425f633b191af16a6900d
vaishali86c <vaishali86c@gmail.com>
2024-06-03 17:15:28 +0530

    Practice useMemo

--- 09-useMemo/my-app/src/App.jsx
+++ 09-useMemo/my-app/src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 function App() {
 
   const [counter, setCounter] = useState(0);
   const [inputValue, setInputValue] = useState(1);
 
-  let count = 0;
-  for (let i = 1; i <= inputValue; i++) {
-    count = count + i;
-  }
+
+  let count = useMemo(() => {
+    let finalcount = 0;
+    for (let i = 1; i <= inputValue; i++) {
+      finalcount = finalcount + i;
+    }
+    return finalcount;
+  }, [inputValue]);
+  
 
   return (
     <>
