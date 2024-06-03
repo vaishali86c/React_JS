9cb26c72713364ffa0640bbec3019464bc4b0655
vaishali86c <vaishali86c@gmail.com>
2024-06-03 17:50:36 +0530

    Practice usecallback

---
+++ 10-useCallback/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState, useCallback, memo } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const inputFunction = useCallback(() => {
+    console.log("hi there");
+  }, []);
+
+  return (
+    <div>
+      <ButtonComponent inputFunction={inputFunction} />
+      <button onClick={() => {
+        setCount(count + 1);
+      }}>click me {count}</button>
+    </div>
+  );
+}
+
+const ButtonComponent = memo(({ inputFunction }) => {
+  console.log("child render");
+  return (
+    <div>
+      <button onClick={inputFunction}>button clicked</button>
+    </div>
+  );
+});
+
+export default App;
