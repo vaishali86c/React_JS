6a66a57b46276793d8967bab3fcc43779b07c11d
vaishali86c <vaishali86c@gmail.com>
2024-06-04 17:48:10 +0530

    Add style using chakra ui and external css

--- 11-Api-Integration-Login-Signup-ClientSide/my-app/src/pages/LoginForm.js
+++ 11-Api-Integration-Login-Signup-ClientSide/my-app/src/pages/LoginForm.js
@@ -1,68 +1,77 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Box, FormControl, FormLabel, Input, Button, Text } from "@chakra-ui/react"; 
+import "../styles/LoginForm.css";
+
 
 function LoginForm() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const [formData, setFormData] = useState({
-        email: "",
-        password: "",
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
     });
+  };
 
-    const handleChange = (event) => {
-        setFormData({
-          ...formData,
-          [event.target.name]: event.target.value,
-        });
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-    
-        try {
-          const response = await axios.post(
-            'http://162.240.148.130:8030/auth/clientlogin',
-            formData,
-            {
-              headers: { 'Content-Type': 'application/json' },
-            }
-          );
-          console.log('Success:', response.data);
-        } catch (error) {
-          console.error('Error:', error.response.data || error.message);
+    try {
+      const response = await axios.post(
+        'http://162.240.148.130:8030/auth/clientlogin',
+        formData,
+        {
+          headers: { 'Content-Type': 'application/json' },
         }
-    };
+      );
+      console.log('Success:', response.data);
+    } catch (error) {
+      console.error('Error:', error.response.data || error.message);
+    }
+  };
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <h2>Login Form</h2>
-            <div className="form-group">
-                <label>Email:</label>
-                <input
-                type="email"
-                name="email"
-                id="email"
-                value={formData.email}
-                onChange={handleChange}
-                />
-            </div>
-            <div className="form-group">
-                <label>Password:</label>
-                <input
-                type="password"
-                name="password"
-                id="password"
-                value={formData.password}
-                onChange={handleChange}
-                />
-            </div>
-            <button type="submit">Login</button>
-            <br />
-            <button onClick={() => navigate("/signup")}>Register Here</button>
-        </form>
-    );
+  return (
+    <Box as="form" onSubmit={handleSubmit} className="login-form" w={600} h={500}> 
+      <h1>Login Form</h1>
+      <FormControl isRequired>
+        <FormLabel htmlFor="email">Email:</FormLabel>
+        <Input
+          type="email"
+          name="email"
+          id="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel htmlFor="password">Password:</FormLabel>
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </FormControl>
+      <Button type="submit" colorScheme="blue" h={10}>
+        Login
+      </Button>
+      <br />
+      <Text>
+        Don't have an account?{" "}
+        <Button colorScheme="teal" variant="outline" onClick={() => navigate("/signup")}>
+          Register Here
+        </Button>
+      </Text>
+    </Box>
+  );
 }
 
 export default LoginForm;
--- 11-Api-Integration-Login-Signup-ClientSide/my-app/src/pages/SignupForm.js
+++ 11-Api-Integration-Login-Signup-ClientSide/my-app/src/pages/SignupForm.js
@@ -1,107 +1,112 @@
 import { useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Box, FormControl, FormLabel, Input, Button, Text } from "@chakra-ui/react"; 
+import "../styles/SignupForm.css";
 
-function RegisterForm() {
+function SignupForm() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+    name: "",
+    phone_no: "",
+    date_of_birth: "",
+  });
 
-    const [formData, setFormData] = useState({
-        email: "", 
-        password: "",
-        name: "",
-        phone_no: "",
-        date_of_birth: "",
-      });
+  const handleChange = (event) => {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-      const handleChange = (event) => {
-        setFormData({
-            ...formData,
-            [event.target.name]: event.target.value,
-        });
-      };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
+    try {
+      const response = await axios.post(
+        'http://162.240.148.130:8030/auth/clientregister',
+        formData,
+        {
+          headers: { 'Content-Type': 'application/json' },
+        }
+      );
+      console.log('Success:', response.data);
      
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-    
-        try {
-            const response = await axios.post(
-                'http://162.240.148.130:8030/auth/clientregister',
-                formData,
-                {
-                    headers: { 'Content-Type': 'application/json' },
-                }
-            );
-                console.log('Success:', response.data);
-            
-        } catch (error) {
-                console.error('Error:', error.response.data || error.message);
-        };
+    } catch (error) {
+      console.error('Error:', error.response.data || error.message);
+     
     }
-    const navigate = useNavigate();
+  };
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <h2>Registration Form</h2>
-            <div className="form-group">
-                <label>Email:</label>
-                <input
-                type="email"
-                name="email"
-                id="email"
-                value={formData.email}
-                onChange={handleChange}
-                />
-            </div>
-            <div className="form-group">
-                <label>Password:</label>
-                <input
-                type="password"
-                name="password"
-                id="password"
-                value={formData.password}
-                onChange={handleChange}
-                />
-            </div>
-            
-            <div className="form-group">
-                <label>Name:</label>
-                <input
-                type="text"
-                name="name"
-                id="name"
-                value={formData.name}
-                onChange={handleChange}
-                />
-            </div>
-            <div className="form-group">
-                <label>Phone Number:</label>
-                <input
-                type="tel"
-                name="phone_no"
-                id="phone_no"
-                value={formData.phone_no}
-                onChange={handleChange}
-                pattern="[0-9]{10}"
-                />
-            </div>
-            <div className="form-group">
-                <label>Date of Birth:</label>
-                <input
-                type="date"
-                name="date_of_birth"
-                id="date_of_birth"
-                value={formData.date_of_birth}
-                onChange={handleChange}
-                />
-            </div>
-            <button type="submit">Sign Up</button>
-            <br />
-            <div>
-                <h4>Already have an account?</h4>
-                <button onClick={() => navigate("/login")}>Login Here</button>
-            </div>
-    </form>
-    );
+  const navigate = useNavigate();
+
+  return (
+    <Box as="form" onSubmit={handleSubmit} className="signup-form" w={600} h={700}> 
+      <h1>Registration Form</h1>
+      <FormControl isRequired>
+        <FormLabel htmlFor="email">Email:</FormLabel>
+        <Input
+          type="email"
+          name="email"
+          id="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel htmlFor="password">Password:</FormLabel>
+        <Input
+          type="password"
+          name="password"
+          id="password"
+          value={formData.password}
+          onChange={handleChange}
+        />
+      </FormControl>
+      <FormControl isRequired>
+        <FormLabel htmlFor="name">Name:</FormLabel>
+        <Input
+          type="text"
+          name="name"
+          id="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="phone_no">Phone Number:</FormLabel>
+        <Input
+          type="tel"
+          name="phone_no"
+          id="phone_no"
+          value={formData.phone_no}
+          onChange={handleChange}
+          pattern="[0-9]{10}" 
+        />
+      </FormControl>
+      <FormControl>
+        <FormLabel htmlFor="date_of_birth">Date of Birth:</FormLabel>
+        <Input
+          type="date"
+          name="date_of_birth"
+          id="date_of_birth"
+          value={formData.date_of_birth}
+          onChange={handleChange}
+        />
+      </FormControl>
+      <Button type="submit" colorScheme="teal" h={10}>
+        Sign Up
+      </Button>
+      <br />
+      <Text>
+        Already have an account?{" "}
+        <Button colorScheme="blue" variant="outline" onClick={() => navigate("/login")}>
+          Login Here
+        </Button>
+      </Text>
+    </Box>
+  );
 }
 
-export default RegisterForm;
+export default SignupForm;
